b6d5378891d523dd4fa65536d2fb1c95f4472dcd
DenniseCarcueva <carcuevadennise@gmail.com>
2017-03-02 16:13:50 +0800

    b

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
-app.get('/*', function(req, res){
+app.get('/about', function(req, res){
   res.sendFile(path.join(__dirname, 'views/pmo.html'));
+});
+app.get('*', function(req, res){
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
 //express server listen
 var server = app.listen(app.get('port'), function(){
